320fbf625131d4a0e1706811ab7f4283a319aab1
Joseph Schafer <github@thebitfarm.org>
2015-12-07 01:36:34 -0500

    bashrc

---
+++ bashrc
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o vi
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+
+source /usr/local/git/contrib/completion/git-completion.bash
+source /usr/local/git/contrib/completion/git-prompt.sh
+GIT_PS1_SHOWDIRTYSTATE=true
+export PS1='[\u@mbp \w$(__git_ps1)]\$ '
--- install.sh
+++ install.sh
@@ -13,12 +13,14 @@ rm ~/.gitconfig
 rm ~/.gitignore
 rm ~/.tmux.conf
 rm ~/.pip/pip.conf
+rm ~/.bashrc
 
 echo "Symlinking files"
 ln -s ~/Data/projects/dotfiles/vimrc ~/.vimrc
 ln -s ~/Data/projects/dotfiles/zshrc ~/.zshrc
 ln -s ~/Data/projects/dotfiles/vim ~/.vim
 ln -s ~/Data/projects/dotfiles/gvimrc ~/.gvimrc
+ln -s ~/Data/projects/dotfiles/bashrc ~/.bashrc
 ln -s ~/Data/projects/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/Data/projects/dotfiles/gitignore ~/.gitignore
 ln -s ~/Data/projects/dotfiles/tmux ~/.tmux.conf
@@ -30,4 +32,3 @@ echo "Updating submodules"
 git submodule foreach git pull origin master --recurse-submodules
 
 echo "All done."
-
